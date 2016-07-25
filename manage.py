fc9e7d2fef9de755879e4c5fe186b3bc7804c99d
jessehartloff <hartloff@buffalo.edu>
2016-07-25 13:54:08 -0400

    Add interview questions for 442

--- manage.py
+++ manage.py
@@ -2,6 +2,13 @@
 import os
 import sys
 
+'''
+ASDFAguohqerf
+eagrioaw'
+awefu
+fqeirojfha
+afwruohasf
+'''
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "courseWebsite.settings")
 
