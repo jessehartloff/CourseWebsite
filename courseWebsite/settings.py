74b6f1b234cb5fcfa49a2962baca11578cedc54d
jessehartloff <hartloff@buffalo.edu>
2016-07-25 15:12:33 -0400

    Add interview questions for 442

--- courseWebsite/settings.py
+++ courseWebsite/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # STATIC_ROOT = '/http://www.cse.buffalo.edu/~hartloff/CourseWebsite/lecture/'
 
 # STATIC_URL = '/static/'
-STATIC_URL = 'http://yeager.cse.buffalo.edu/~hartloff/CourseWebsite/static/'
+STATIC_URL = 'http://www.cse.buffalo.edu/~hartloff/CourseWebsite/static/'
 # STATIC_URL = 'http://google.com'
 
 # STATICFILES_DIRS = [
