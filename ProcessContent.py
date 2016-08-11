b1b16eba96de6c0b3cf91abafe009fb6a4b0b423
jessehartloff <hartloff@buffalo.edu>
2016-08-11 12:44:21 -0400

    Testing web hook

--- ProcessContent.py
+++ ProcessContent.py
@@ -12,3 +12,4 @@ process_course('CSE404', "Web Development", image_destination_directory)
 process_course('CSE442', "Software Engineering", image_destination_directory)
 process_course('CSE442-Fall', "Software Engineering", image_destination_directory)
 
+### Did it work???
