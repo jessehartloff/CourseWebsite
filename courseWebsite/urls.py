7208ad492261309b84a0241faf3d440b84896814
jessehartloff <hartloff@buffalo.edu>
2016-08-16 16:18:56 -0400

    production mode

--- courseWebsite/urls.py
+++ courseWebsite/urls.py
@@ -19,7 +19,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^courses/', include('courses.urls', namespace="courses")),
-    url(r'^admin/', admin.site.urls),
-    url(r'^admin', admin.site.urls),
+    # url(r'^admin/', admin.site.urls),
+    # url(r'^admin', admin.site.urls),
     url(r'^', TemplateView.as_view(template_name='faculty_page.html')),
 ]
