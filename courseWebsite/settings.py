0ce709d92511973eaad741f7b1086b45d384a31c
jessehartloff <hartloff@buffalo.edu>
2016-08-16 12:40:41 -0400

    fix settings

--- courseWebsite/settings.py
+++ courseWebsite/settings.py
@@ -82,18 +82,18 @@ WSGI_APPLICATION = 'courseWebsite.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'django_courses',
-    #     'USER': 'django',
-    #     'PASSWORD': 'donthackmeplease',
-    #     'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
-    #     'PORT': '3306',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_courses',
+        'USER': 'django',
+        'PASSWORD': 'donthackmeplease',
+        'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+    }
 }
 
 # Password validation
