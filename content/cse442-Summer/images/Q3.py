d3c0769baadfd209c6620ab9483bb3ef1c5dca67
jessehartloff <hartloff@buffalo.edu>
2016-08-16 16:49:31 -0400

    dammit git!

---
+++ content/cse442-Summer/images/Q3.py
@@ -0,0 +1,18 @@
+
+class MinStack:
+
+    # add data to the top of the stack
+    def push(self, data):
+        pass
+
+    # remove the top element from the stack. Does not return any value
+    def pop(self):
+        pass
+
+    # returns the top element of the stack. Does not remove the top element
+    def top(self):
+        pass
+
+    # returns the minimum value in the stack according to the elements' less than operator
+    def min(self):
+        pass
