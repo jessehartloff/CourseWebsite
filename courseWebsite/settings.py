7165b2e7acc7a3d7ef2c19f88229fe9150b949db
jessehartloff <hartloff@buffalo.edu>
2016-10-20 15:43:30 -0400

    Update repo links

--- courseWebsite/settings.py
+++ courseWebsite/settings.py
@@ -139,8 +139,8 @@ USE_TZ = True
 # STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
 # STATIC_ROOT = '/http://www.cse.buffalo.edu/~hartloff/CourseWebsite/lecture/'
 
-# STATIC_URL = '/static/'
-STATIC_URL = 'http://www.cse.buffalo.edu/~hartloff/CourseWebsite/static/'
+STATIC_URL = '/static/'
+# STATIC_URL = 'http://www.cse.buffalo.edu/~hartloff/CourseWebsite/static/'
 # STATIC_URL = 'http://google.com'
 
 # STATICFILES_DIRS = [
